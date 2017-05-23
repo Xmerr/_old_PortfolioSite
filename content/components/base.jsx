6cb5ee32bcb66fb651ef253f7e2192adea233522
Wiley Hilton <wileyhilton@gmail.com>
2017-05-23 14:58:30 +0000

    Added a scroll animation

--- content/components/base.jsx
+++ content/components/base.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var splash = require('../images/splash.webp');
-
 require('./base.scss');
 
+var scroll = require('scroll-into-view');
+
 class Base extends React.Component{
+    componentDidMount() {
+        setTimeout(() => {
+            scroll(this.refs.intro, {
+               time: 1000
+            });
+        }, 5000);
+    }
+    
     render() {
         return(
             <div className='MainArea'>
                 <div className="splash">
-                    <img src={splash} draggable='false' />
                     <span className="quote">
-                        “Why fit in when you're born to stand out?”
+                        Why fit in when you're born to stand out?
                         <br />
                         - Dr. Suess
                     </span>
+                </div>
+                <div ref="intro">
+                    <h1>Wiley Hilton</h1>
                 </div>
             </div>
         );
