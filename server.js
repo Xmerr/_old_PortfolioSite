46e95c6c653b8bc88b0106639c94c7bb5b540804
Wiley Hilton <wileyhilton@gmail.com>
2017-09-26 20:56:43 +0000

    broke socketio

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
 var path = require('path');
 
-process.env = {
-    port: 80
-};
-
-(() => {
-    var os = require('os');
-    if(os.hostname().indexOf('xmer') !== -1) {
-        // Determine if it's my test server or not
-        process.env.port = 8080;
-    }
-})();
-
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
@@ -29,12 +17,10 @@ app.get('/*', (req, res) => {
    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-server.listen(process.env.port);
-
 io.on('connection', function(socket) {
     socket.clientData = [];
     socket.clientData[socket.handshake.issued] = {};
     socketFiles(socket);
 });
 
-module.exports = server;
+module.exports = app;
