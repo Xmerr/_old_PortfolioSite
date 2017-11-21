92d8b2f524067428ce18f9b9483b4145cbf0ccb4
Wiley Hilton <wileyhilton@gmail.com>
2017-11-21 18:56:40 +0000

    modified site

--- content/.redux/reducers.jsx
+++ content/.redux/reducers.jsx
@@ -2,12 +2,8 @@ import { combineReducers } from 'redux';
 import { actions } from './actions';
 
 import Splash from '../components/splash/splash';
-import SpaceShooter from '../components/games/SpaceShooter/game';
 
 const component = (state = <Splash key="Splash" />, action) => {
-    if(location.pathname.indexOf('testing') >= 0) {
-        state = <SpaceShooter key="gw" />;
-    }
     switch (action.type) {
         case actions.CHANGE_COMPONENT:
             return action.component;
