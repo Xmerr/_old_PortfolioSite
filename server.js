dafc6a1c520ba2e74f9afcfe4bf4e157171ab0be
Wiley Hilton <wileyhilton@gmail.com>
2018-03-05 15:49:09 +0000

    updated to new layout

--- server.js
+++ server.js
@@ -3,23 +3,27 @@ var path = require('path');
 var express = require('express'),
     app = express(),
     socketFiles = require('./socketFiles.js');
-    
-app.use(require('prerender-node'));
-    
-app.use('/public', express.static(path.join(__dirname, '.public'),{
-    index: false,
-    extensions: ['js', 'html']
-}));
 
-app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname + '/index.html'));
-});
+module.exports = cb => {
+    app.use(require('prerender-node'));
 
-module.exports.app = app;
-module.exports.io = io => {
-    io.on('connection', function(socket) {
-        socket.clientData = [];
-        socket.clientData[socket.handshake.issued] = {};
-        socketFiles(socket);
+    app.use('/public', express.static(path.join(__dirname, '.public'),{
+        index: false,
+        extensions: ['js', 'html']
+    }));
+
+    app.get('/*', (req, res) => {
+       res.sendFile(path.join(__dirname + '/index.html'));
+    });
+
+    cb({
+        app,
+        io: io => {
+            io.on('connection', function(socket) {
+                socket.clientData = [];
+                socket.clientData[socket.handshake.issued] = {};
+                socketFiles(socket);
+            });
+        }
     });
 };
